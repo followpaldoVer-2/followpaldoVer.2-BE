38a575db0e14b31f2fa36ddf03276878e8e0601c
haha0ha <haha0hazzang@gmail.com>
2024-05-29 20:35:04 +0900

    프로젝트 환경설정

---
+++ src/test/java/jjon/followpaldo2/Followpaldo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jjon.followpaldo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Followpaldo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
